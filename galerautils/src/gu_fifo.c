cb5eb13d090b1ed8b340193bd989822a33e67513
Otto Kekäläinen <otto@debian.org>
2020-05-29 23:37:43 +0300

    New upstream version 26.4.5

--- galerautils/src/gu_fifo.c
+++ galerautils/src/gu_fifo.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2017 Codership Oy <info@codership.com>
+ * Copyright (C) 2008-2020 Codership Oy <info@codership.com>
  *
  * Queue (FIFO) class implementation
  *
@@ -339,8 +339,9 @@ void* gu_fifo_get_head (gu_fifo_t* q, int* err)
     }
 }
 
-/*! Advances FIFO head and unlocks FIFO. */
-void gu_fifo_pop_head (gu_fifo_t* q)
+/*! Unprotected helper for gu_fifo_pop_head() and gu_fifo_clear() */
+static inline
+void fifo_advance_head (gu_fifo_t* q)
 {
     if (FIFO_COL(q, q->head) == q->col_mask) {
         /* removing last unit from the row */
@@ -356,6 +357,12 @@ void gu_fifo_pop_head (gu_fifo_t* q)
     if (gu_unlikely(q->used < q->used_min)) {
         q->used_min = q->used;
     }
+}
+
+/*! Advances FIFO head and unlocks FIFO. */
+void gu_fifo_pop_head (gu_fifo_t* q)
+{
+    fifo_advance_head(q);
 
     if (fifo_unlock_get(q)) {
         gu_fatal ("Faled to unlock queue to get item.");
@@ -469,6 +476,15 @@ void gu_fifo_stats_flush(gu_fifo_t* q)
     fifo_unlock (q);
 }
 
+void gu_fifo_clear(gu_fifo_t* q)
+{
+    fifo_lock (q);
+
+    while (q->used > 0) fifo_advance_head(q);
+
+    fifo_unlock (q);
+}
+
 /* destructor - would block until all members are dequeued */
 void gu_fifo_destroy   (gu_fifo_t *queue)
 {
@@ -498,7 +514,7 @@ void gu_fifo_destroy   (gu_fifo_t *queue)
 
     while (gu_mutex_destroy (&queue->lock)) continue;
 
-    /* only one row migth be left */
+    /* only one row might be left */
     {
         ulong row = FIFO_ROW(queue, queue->tail);
         if (queue->rows[row]) {
